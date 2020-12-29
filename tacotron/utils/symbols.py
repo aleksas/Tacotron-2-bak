d8cda11d2db1700eef2af901a92170138bdd270c
Aleksas Pielikis <ant.kampo@gmail.com>
2020-12-29 02:08:19 +0200

    Update symbols.py

--- tacotron/utils/symbols.py
+++ tacotron/utils/symbols.py
@@ -8,7 +8,7 @@ from . import cmudict
 
 _pad        = '_'
 _eos        = '~'
-_characters = 'AĄBCČDEĘĖFGHIĮJKLMNOPQRSŠTUŲŪVWXYZŽaąbcčdeęėfghiįjklmnopqrsštuųūvwxyzž!\'(),-.:;? \x0300\x0301\x0303'
+_characters = 'AĄBCČDEĘĖFGHIĮJKLMNOPQRSŠTUŲŪVWXYZŽaąbcčdeęėfghiįjklmnopqrsštuųūvwxyzž!\'(),-.:;? \u0300\u0301\u0303'
 
 # Export all symbols:
 symbols = [_pad, _eos] + list(_characters)
