a1f5e8e66cf6050616120b78d4f70aa271618a2d
Aleksas Pielikis <ant.kampo@gmail.com>
2021-06-10 20:36:57 +0000

    Add helper files

---
+++ preprocess.sh
@@ -0,0 +1,4 @@
+python preprocess.py --dataset='liepa-multi' --voice='Regina' --output='training_data_Regina'
+python preprocess.py --dataset='liepa-multi' --voice='Aiste' --output='training_data_Aiste'
+python preprocess.py --dataset='liepa-multi' --voice='Edvardas' --output='training_data_Edvardas'
+python preprocess.py --dataset='liepa-multi' --voice='Vladas' --output='training_data_Vladas'
