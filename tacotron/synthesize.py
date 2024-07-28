caccc8b934f1191035bf7dc9234ca8ce7c012c7f
Aleksas Pielikis <ant.kampo@gmail.com>
2024-07-28 16:28:40 +0300

    fix encoding

--- tacotron/synthesize.py
+++ tacotron/synthesize.py
@@ -57,7 +57,7 @@ def run_eval(args, checkpoint_path, output_dir, hparams, sentences):
 	synth.load(checkpoint_path, hparams)
 
 
-	with open(os.path.join(eval_dir, 'map.txt'), 'w') as file:
+	with open(os.path.join(eval_dir, 'map.txt'), 'w', encoding='utf-8') as file:
 		for i, text in enumerate(tqdm(sentences)):
 			start = time.time()
 			mel_filename, speaker_id = synth.synthesize([text], [i+1], eval_dir, log_dir, None)
