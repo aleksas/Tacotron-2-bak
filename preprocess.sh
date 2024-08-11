5e7a16c74633823d24fd0fb64c436573a36cd580
Aleksas Pielikis <ant.kampo@gmail.com>
2024-08-11 11:34:17 +0000

    Update training

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,11 @@
+wget https://github.com/aleksas/liepa_dataset/blob/master/other/regina_transcript_stressed.txt -O Regina/db_tr.txt
 python preprocess.py --dataset='liepa-multi' --voice='Regina' --output='training_data_Regina'
+
+wget https://github.com/aleksas/liepa_dataset/blob/master/other/aiste_transcript_stressed.txt -O Aiste/db_tr.txt
 python preprocess.py --dataset='liepa-multi' --voice='Aiste' --output='training_data_Aiste'
+
+wget https://github.com/aleksas/liepa_dataset/blob/master/other/edvardas_transcript_stressed.txt -O Edvardas/db_tr.txt
 python preprocess.py --dataset='liepa-multi' --voice='Edvardas' --output='training_data_Edvardas'
+
+wget https://github.com/aleksas/liepa_dataset/blob/master/other/vladas_transcript_stressed.txt -O Vladas/db_tr.txt
 python preprocess.py --dataset='liepa-multi' --voice='Vladas' --output='training_data_Vladas'
